47b47880e86a2e9953817fcc90d5ec3f2e1bab29
Christian Jans <cjjans@ualberta.ca>
2023-08-22 13:53:28 +0000

    Change error message

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -9,6 +9,6 @@ export const NEIGHBOURHOOD_SUBTITLE = "Get related words";
 export const NEIGHBOURHOOD_PROMPT = "Word:";
 export const NEIGHBOURHOOD_NUMBER_OF_RESULTS = "Number of results:"
 
-export const INPUT_WORD_ERROR = "Please enter a word. (no letters, spaces, or symbols)"
-export const INPUT_NUMBER_ERROR_0 = "Please enter an integer greater than 0.";
-export const INPUT_NUMBER_ERROR_5 = "Please enter an integer greater than 5.";
+export const INPUT_WORD_ERROR = "Please enter a word (no numbers or symbols, up to two words)"
+export const INPUT_NUMBER_ERROR_0 = "Please enter an integer greater than 0";
+export const INPUT_NUMBER_ERROR_5 = "Please enter an integer greater than 5";
